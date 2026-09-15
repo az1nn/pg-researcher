353cdcc71827887efdaf427642b84abb76c87fcd
Alan <80423834+az1nn@users.noreply.github.com>
2026-09-15 14:06:00 -0300

    feat(religion): add religion corpus package

---
+++ src/pg_researcher/religion/__init__.py
@@ -0,0 +1,13 @@
+from pg_researcher.religion.models import (
+    ComparativeParallel,
+    Manuscript,
+    ReligionSource,
+    RightsRecord,
+)
+
+__all__ = [
+    "ComparativeParallel",
+    "Manuscript",
+    "ReligionSource",
+    "RightsRecord",
+]
